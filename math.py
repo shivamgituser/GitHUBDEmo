0a41b2fc84ad89bdca456a610ca2596686d38658
shivam <shivampersonalmail8@gmail.com>
2021-06-03 14:46:17 +0530

    added fn header

--- math.py
+++ math.py
@@ -1,8 +1,17 @@
+
+#add impplemetation
 def add(x,y):
     pass
+
+# subtarct implementation
 def subtract(x,y):
     pass
+
+# multiplication implemetation
+
 def multiply(x,y):
     pass
+
+ # division implementation
 def divide(x,y):
     pass
